b846634f2f97ff750920fc4c81c5d02ab79316fb
Igor <iperun23@gmail.com>
2024-01-31 09:12:31 -0500

    mobile changes

--- components/Layout.js
+++ components/Layout.js
@@ -23,7 +23,9 @@ const Layout = ({ children }) => {
 			<div className="flex lg:justify-end absolute justify-center z-10 w-full right-[3%] top-[5%]">
 				<button
 					className={`${
-						theme === "red" ? "bg-blue-200 text-black" : "bg-red-400 text-black"
+						theme === "red"
+							? "lg:bg-blue-200 bg-blue-600 text-black"
+							: "lg:bg-red-400 bg-red-600 text-black"
 					} flex items-center gap-2 px-4 py-2  rounded-md  dark:bg-gray-800`}
 					onClick={toggleTheme}
 				>
